6630290c9a518bcac0e2ba47f1a56a32224f2e00
lj <2263945042@qq.com>
2025-02-06 15:30:10 +0800

    发布

--- src/components/utils/SideNav.js
+++ src/components/utils/SideNav.js
@@ -1,34 +1,15 @@
 import React from "react";
 import {Nav,Accordion,useAccordionButton } from "react-bootstrap"
 
-// function SideNavSubItem(props) {
-//     return (
-//         <Accordion.Collapse eventKey={props.eventKey}>
-//             <Nav.Link>{props.title}</Nav.Link>
-//         </Accordion.Collapse>
-//     );
-// }
-
 function SideNavItem(props) {
-    // if (!props.subItems || props.subItems.length===0){
-    //     return (
-    //         <Accordion.Item eventKey={props.eventKey}>
-    //             <Nav.Link flush>{props.title}</Nav.Link>
-    //         </Accordion.Item>
-    //     )
-    // }
-    // return (
-    //     <Accordion.Item eventKey={props.eventKey}>
-    //         <Accordion.Header>{props.title}</Accordion.Header>
-    //         {props.subItems.map((subItem, index) => (
-    //             <SideNavSubItem key={index} {...subItem} />
-    //         ))}
-    //     </Accordion.Item>
-    // );
+
     
     return (
         <Accordion.Item eventKey={props.eventKey}>
-            <Nav.Link onClick={()=>{
+            <Nav.Link 
+                className={props.currEventKey === props.eventKey ? 'side-nav-activate' : ''}
+                onClick={()=>{
+                props.setCurrEventKey(props.eventKey);
                 props.setEventKey(props.eventKey);
                 props.setActivate(1);
                 window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -38,33 +19,12 @@ function SideNavItem(props) {
 }
 
 export default function SideNav(props){
-    // const items = [
-    //     {
-    //         title: "Front",
-    //         eventKey: "0",
-    //         subItems: [
-    //             { title: "React", eventKey: "0" },
-    //             { title: "Bootstrap", eventKey: "0" }
-    //         ]
-    //     },
-    //     {
-    //         title: "Back End",
-    //         eventKey: "1",
-    //         subItems: [
-    //             { title: "Spring", eventKey: "1" }
-    //         ]
-    //     },
-    //     {
-    //         title: "Other",
-    //         eventKey: "2",
-    //         subItems: []
-    //     }
-    // ];    
+    const [currEventKey, setCurrEventKey] = React.useState(0);
     const sideNavHtml=(
         <Accordion className="fixed-accordion">
             <Nav className="flex-column">
                 {props.data.map((notebook, index) => (
-                    <SideNavItem className="side-nav-item" key={index} {...notebook} setEventKey={props.setEventKey} setActivate={props.setActivate}/>
+                    <SideNavItem className="side-nav-item" currEventKey={currEventKey} setCurrEventKey={setCurrEventKey} key={index} {...notebook} setEventKey={props.setEventKey} setActivate={props.setActivate}/>
                 ))}
             </Nav>
          </Accordion>
